7c3fb5781e1b7666c1390b4d03254bab231b796d
Lettierydl <lettierydl@gmail.com>
2014-02-27 11:22:02 -0300

    Correção do Scrapy

--- Scraper/hotel/scraper_app/spiders/hotel_spider.py
+++ Scraper/hotel/scraper_app/spiders/hotel_spider.py
@@ -1,13 +1,7 @@
-from _sqlite3 import Cache
-import json
-import time
-
-from scrapy.contrib.linkextractors.sgml import BaseSgmlLinkExtractor
-from scrapy.http.request.form import FormRequest
 from scrapy.selector import Selector
-from scrapy.selector.lxmlsel import HtmlXPathSelector
 from scrapy.spider import BaseSpider
-from selenium import webdriver
+
+import json
 
 
 class ComentariosSpider(BaseSpider):
@@ -19,55 +13,28 @@ class ComentariosSpider(BaseSpider):
     
     deals_list_xpath = '//div[@class="reviewSelector"]'
     
-    
-    def __init__(self):
-        self.driver = webdriver.Firefox()
-    
     def parse(self, response):
-        self.driver.get(response.url)
+        selector = Selector(response)
+        autores = selector.xpath('.//div[@class="username mo"]/span/text()').extract()
+        descricoes = selector.xpath('.//div[@class="entry"]/p[@class="partial_entry"]/text()').extract()
         
+        listaDeComentarios =[]
         
+        cont = 0;
+        while cont < descricoes.__len__():
+            if descricoes[cont]=="\n":
+                descricoes.remove("\n")
+                continue
+            cont += 1
+            
+            listaDeComentarios.append({'autor': autores[cont], 'descricao' : descricoes[cont]})
+            
+            cont += 1
         
-        link = self.driver.find_element_by_xpath('//*[@id="HSCS"]/table/tbody/tr[1]/td[1]/a')
-        link.click()
-        
-        try:
-            
-            time.sleep(5)
-            
-            selector = Selector(response)
+        jsonString = json.JSONEncoder().encode(listaDeComentarios)  
+        jsonArquivo = open('Comentarios.json', "w")
+        jsonArquivo.write(jsonString)
+        jsonArquivo.close()
             
             
-            autores = selector.xpath('.//div[@class="username mo"]/span/text()').extract() 
-            descricoes = selector.xpath('.//div/div[@class="entry"]/p/text()').extract() 
-            """
-            descricoes = self.driver.find_elements_by_xpath('//*[@id="UR195186188"]/div[2]/div/div[3]/p/text()')
-            autores = selector.xpath('.//div[@class="username mo"]/span/text()').extract() """
-          
-            
-            listaDeComentarios =[]
-            print autores.__len__()
-            print descricoes.__len__()
-            
-            cont = 0;
-            while cont < descricoes.__len__():
-                if descricoes[cont]=="\n":
-                    descricoes.remove("\n")
-                    continue
-                print cont
-                print autores[cont]
-                print descricoes[cont]
-                
-                listaDeComentarios.append({'autor': autores[cont], 'descricao' : descricoes[cont]})
-                
-                cont += 1
-        
-            """
-            jsonString = json.JSONEncoder().encode(listaDeComentarios)  
-            jsonArquivo = open('Comentarios.json', "w")
-            jsonArquivo.write(jsonString)
-            jsonArquivo.close()
-            """
-        finally:
-            print "a"
             
